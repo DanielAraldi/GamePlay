5a664b828c88463ef223e8baa89a1561dafff175
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-02-19 18:27:20 -0300

    chore: created import images and theme with your types

---
+++ src/config/images.ts
@@ -0,0 +1,4 @@
+import ILLUSTRATION from '../assets/illustration.png';
+import DISCORD from '../assets/discord.png';
+
+export { ILLUSTRATION, DISCORD };
---
+++ src/config/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    background: '#0D133D',
+    heading: '#DDE3F0',
+    transparent: 'transparent',
+    primary: '#E51C44',
+    line: '#991F36',
+  },
+};
