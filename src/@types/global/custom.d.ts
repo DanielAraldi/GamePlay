2eb4e18147155c7e686f0536d5c211306ced1920
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-02-20 17:48:34 -0300

    feat: add typescript configuration to svgs file

--- src/@types/global/custom.d.ts
+++ src/@types/global/custom.d.ts
@@ -1,1 +1,11 @@
-declare module '*.png';
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
