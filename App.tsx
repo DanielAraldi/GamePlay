186923e58788c96c3e84a7b3e2b9aacadfca4eca
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-02-20 10:32:20 -0300

    feat: add loading to fonts and visualization configuration to splash screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,38 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from '@expo-google-fonts/rajdhani';
+
 import { theme } from './src/config';
 import { SignIn } from './src/screens';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      async function hideSplashScreen(): Promise<void> {
+        await SplashScreen.hideAsync();
+      }
+
+      hideSplashScreen();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
     <>
       <StatusBar
