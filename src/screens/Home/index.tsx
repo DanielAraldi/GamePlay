2fcf22fad974d9e58d59c72e81d53e96b889a392
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-02-20 17:50:26 -0300

    feat: add CategorySelect on Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import { View } from 'react-native';
 
-import { ButtonAdd, Profile } from '../../components';
+import { ButtonAdd, CategorySelect, Profile } from '../../components';
 import { styles } from './styles';
 
 export function Home() {
+  const [category, setCategory] = useState<string>('');
+
+  function handleCategorySelect(categoryId: string): void {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Profile />
 
         <ButtonAdd activeOpacity={0.7} />
+      </View>
+
+      <View>
+        <CategorySelect
+          categorySelected={category}
+          onSelect={handleCategorySelect}
+        />
       </View>
     </View>
   );
