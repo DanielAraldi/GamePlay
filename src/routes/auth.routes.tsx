5348bd1b0fbf6fe89e94153339212ca28576a604
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-02-20 13:24:53 -0300

    feat: created routes

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,23 @@
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Home, SignIn } from '../screens';
+import { theme } from '../config';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AuthRoutes() {
+  return (
+    <Navigator
+      initialRouteName='SignIn'
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: theme.colors.secondary100,
+        },
+      }}
+    >
+      <Screen name='SignIn' component={SignIn} />
+      <Screen name='Home' component={Home} />
+    </Navigator>
+  );
+}
