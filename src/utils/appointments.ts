def7233eb97b23a7c36f8dab7402310894a3fd78
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-02-20 19:12:13 -0300

    feat: created appointments mocks

---
+++ src/utils/appointments.ts
@@ -0,0 +1,30 @@
+import { AppointmentProps } from '../@types';
+
+export const APPOINTMENTS: AppointmentProps[] = [
+  {
+    category: '1',
+    date: '22/06 às 20:40h',
+    description:
+      'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    guild: {
+      icon: null,
+      id: '1',
+      name: 'Lendários',
+      owner: true,
+    },
+    id: '1',
+  },
+  {
+    category: '1',
+    date: '22/06 às 20:40h',
+    description:
+      'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    guild: {
+      icon: null,
+      id: '1',
+      name: 'Lendários',
+      owner: true,
+    },
+    id: '2',
+  },
+];
