408ce18cf2699e6bb35791f14c1e76851f1e5c8f
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-02-20 13:25:44 -0300

    feat: starting structuring and styling the Home header

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import { View } from 'react-native';
+
+import { Profile } from '../../components';
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Profile />
+      </View>
+    </View>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  header: {
+    flexDirection: 'row',
+
+    width: '100%',
+
+    justifyContent: 'space-between',
+
+    marginTop: 10,
+    marginBottom: 42,
+
+    paddingHorizontal: 24,
+  },
+});
