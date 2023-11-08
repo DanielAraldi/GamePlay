1b7e8a5376907cbec8144b2f1c87bc1a284b5a31
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-11-08 13:49:38 -0300

    fix: added close modal event to touch out `ModalView` component

--- src/components/ModalView/index.tsx
+++ src/components/ModalView/index.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import { Modal, View } from 'react-native';
+import { Modal, View, TouchableWithoutFeedback } from 'react-native';
 
 import { ModalViewProps } from '../../@types';
 import { Background } from '../Background';
 
 import { styles } from './styles';
 
-export function ModalView({ children, ...rest }: ModalViewProps) {
-  return (
-    <Modal transparent animationType='slide' {...rest}>
-      <View style={styles.overlay}>
-        <View style={styles.container}>
-          <Background>
-            <View style={styles.bar} />
+export function ModalView({ children, onDismiss, ...rest }: ModalViewProps) {
+  function handleCloseModal(): void {
+    if (onDismiss) return onDismiss();
+  }
 
-            {children}
-          </Background>
+  return (
+    <Modal transparent statusBarTranslucent animationType='slide' {...rest}>
+      <TouchableWithoutFeedback onPress={handleCloseModal}>
+        <View style={styles.overlay}>
+          <View style={styles.container}>
+            <Background>
+              <View style={styles.bar} />
+
+              {children}
+            </Background>
+          </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 }
