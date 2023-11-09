032afdfc279978a9aa3a5f7cb7d2a9c642efaba6
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-11-09 16:43:53 -0300

    feat: added `SignIn` screen to the not auth route

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,11 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
-import {
-  AppointmentCreate,
-  AppointmentDetails,
-  Home,
-  SignIn,
-} from '../screens';
+import { AppointmentCreate, AppointmentDetails, Home } from '../screens';
 import { theme } from '../config';
 
 const { Navigator, Screen } = createStackNavigator();
@@ -21,7 +16,6 @@ export function AuthRoutes() {
         },
       }}
     >
-      <Screen name='SignIn' component={SignIn} />
       <Screen name='Home' component={Home} />
       <Screen name='AppointmentDetails' component={AppointmentDetails} />
       <Screen name='AppointmentCreate' component={AppointmentCreate} />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 
+import { useAuth } from '../hooks';
+import { SignIn } from '../screens';
+
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
     <NavigationContainer>
-      <AuthRoutes />
+      {user?.id ? <AuthRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 }
